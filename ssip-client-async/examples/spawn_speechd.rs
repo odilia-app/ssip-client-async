e209cf0caf0c13e25b087426e6e5af6a41389d22
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-03-18 08:14:24 -0400

    Add a `with_spawn` method to the fifo struct (#18)
    * with_spawn working
    * remove unnecessary deps
    * fix rustfmt

---
+++ ssip-client-async/examples/spawn_speechd.rs
@@ -0,0 +1,28 @@
+#[cfg(all(unix, not(feature = "async-mio")))]
+use ssip_client_async::{fifo, ClientResult};
+
+#[cfg(all(unix, not(feature = "async-mio")))]
+fn main() -> ClientResult<()> {
+    // spawn the speech-dispatcher daemon before creating the client
+    // and trying to connect to the speech-dispatcher socket
+    let mut client = fifo::Builder::new().with_spawn()?.build()?;
+
+    client
+        .speak()?
+        .check_receiving_data()?
+        .send_line("test message for the spawn example")?
+        .receive_message_id()?;
+
+    client.quit()?.receive()?;
+    Ok(())
+}
+
+#[cfg(all(unix, feature = "async-mio"))]
+fn main() {
+    println!("see async_mio_loop for an example of asynchronous client.");
+}
+
+#[cfg(not(unix))]
+fn main() {
+    println!("example only available on unix.");
+}
