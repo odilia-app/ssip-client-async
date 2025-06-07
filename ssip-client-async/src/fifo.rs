09d395d527b602a2333bac882b0e48d13eb83588
Tait Hoyem <tait@tait.tech>
2025-06-06 20:24:42 -0600

    Remove dirs dep for std::env

--- ssip-client-async/src/fifo.rs
+++ ssip-client-async/src/fifo.rs
@@ -8,6 +8,7 @@
 // modified, or distributed except according to those terms.
 
 use std::io;
+use std::env;
 use std::path::{Path, PathBuf};
 
 const SPEECHD_APPLICATION_NAME: &str = "speech-dispatcher";
@@ -31,8 +32,8 @@ impl FifoPath {
 
     /// Return the standard socket according to the [freedesktop.org](https://www.freedesktop.org/) specification.
     fn default_path() -> io::Result<PathBuf> {
-        match dirs::runtime_dir() {
-            Some(runtime_dir) => Ok(runtime_dir
+        match env::var_os("XDG_RUNTIME_DIR") {
+            Some(runtime_dir) => Ok(PathBuf::from(runtime_dir)
                 .join(SPEECHD_APPLICATION_NAME)
                 .join(SPEECHD_SOCKET_NAME)),
             None => Err(io::Error::new(
